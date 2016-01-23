f06bdc70fb0a69d344a914c3e0e1cc598057740c
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2016-01-23 01:44:58 -0600

    Comclucion de Prueba

--- AsincronoForm/Asincrono.Designer.cs
+++ AsincronoForm/Asincrono.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Asincrono));
             this.AccionUnobutton = new System.Windows.Forms.Button();
             this.AccionDosButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.AcciontextBox = new System.Windows.Forms.TextBox();
+            this.AccionDosTextBox = new System.Windows.Forms.TextBox();
+            this.ReiniciarButton = new System.Windows.Forms.Button();
+            this.AccionTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // AccionUnobutton
@@ -49,6 +53,7 @@
             this.AccionUnobutton.Text = "Accion Uno";
             this.AccionUnobutton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.AccionUnobutton.UseVisualStyleBackColor = true;
+            this.AccionUnobutton.Click += new System.EventHandler(this.AccionUnobutton_Click);
             // 
             // AccionDosButton
             // 
@@ -65,20 +70,55 @@
             this.AccionDosButton.Text = "Accion Dos";
             this.AccionDosButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.AccionDosButton.UseVisualStyleBackColor = true;
+            this.AccionDosButton.Click += new System.EventHandler(this.AccionDosButton_Click);
             // 
-            // textBox1
+            // AcciontextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(139, 112);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(220, 20);
-            this.textBox1.TabIndex = 2;
+            this.AcciontextBox.Location = new System.Drawing.Point(139, 112);
+            this.AcciontextBox.Name = "AcciontextBox";
+            this.AcciontextBox.ReadOnly = true;
+            this.AcciontextBox.Size = new System.Drawing.Size(220, 20);
+            this.AcciontextBox.TabIndex = 2;
+            // 
+            // AccionDosTextBox
+            // 
+            this.AccionDosTextBox.Location = new System.Drawing.Point(139, 149);
+            this.AccionDosTextBox.Name = "AccionDosTextBox";
+            this.AccionDosTextBox.ReadOnly = true;
+            this.AccionDosTextBox.Size = new System.Drawing.Size(220, 20);
+            this.AccionDosTextBox.TabIndex = 3;
+            // 
+            // ReiniciarButton
+            // 
+            this.ReiniciarButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.ReiniciarButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.ReiniciarButton.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ReiniciarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReiniciarButton.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.ReiniciarButton.Image = ((System.Drawing.Image)(resources.GetObject("ReiniciarButton.Image")));
+            this.ReiniciarButton.Location = new System.Drawing.Point(217, 23);
+            this.ReiniciarButton.Name = "ReiniciarButton";
+            this.ReiniciarButton.Size = new System.Drawing.Size(75, 52);
+            this.ReiniciarButton.TabIndex = 4;
+            this.ReiniciarButton.Text = "Reiniciar";
+            this.ReiniciarButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ReiniciarButton.UseVisualStyleBackColor = true;
+            this.ReiniciarButton.Click += new System.EventHandler(this.ReiniciarButton_Click);
+            // 
+            // AccionTimer
+            // 
+            this.AccionTimer.Enabled = true;
+            this.AccionTimer.Interval = 1000;
+            this.AccionTimer.Tick += new System.EventHandler(this.AccionDosButton_Click);
             // 
             // Asincrono
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(527, 267);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ReiniciarButton);
+            this.Controls.Add(this.AccionDosTextBox);
+            this.Controls.Add(this.AcciontextBox);
             this.Controls.Add(this.AccionDosButton);
             this.Controls.Add(this.AccionUnobutton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -93,7 +133,10 @@
 
         private System.Windows.Forms.Button AccionUnobutton;
         private System.Windows.Forms.Button AccionDosButton;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox AcciontextBox;
+        private System.Windows.Forms.TextBox AccionDosTextBox;
+        private System.Windows.Forms.Button ReiniciarButton;
+        public System.Windows.Forms.Timer AccionTimer;
     }
 }
 
--- AsincronoForm/Asincrono.cs
+++ AsincronoForm/Asincrono.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,52 @@ namespace AsincronoForm
         {
             InitializeComponent();
         }
+        private Task<int> EsperaAsync(int seg)
+        {
+            return Task.Run(() => {
+                Thread.Sleep(seg);
+                return seg / 1000;
+
+            });
+        }
+        private Task<int> ContadorAsync(int cont)
+        {
+            return Task.Run(() => {
+                Thread.SpinWait(cont);
+                while (true)
+                {
+                    return cont - 5;
+
+                }
+
+            });
+        }
+      
+        private void Reinciar()
+        {
+            this.AcciontextBox.Clear();
+            this.AccionDosTextBox.Clear();
+            this.Text = "Prueba";
+            this.AccionTimer.Enabled = false;
+        }
+        private async void AccionUnobutton_Click(object sender, EventArgs e)
+        {
+            int segundo = await EsperaAsync(3000);
+            this.AcciontextBox.Text = "Metodo Retornado en " + segundo + "seg";
+            this.Text = "Jose Chavez";
+        }
+
+        private async void AccionDosButton_Click(object sender, EventArgs e)
+        {
+            int contador = await EsperaAsync(10000); //De todos Modos Espera los dos segundo.
+            this.AccionDosTextBox.Text = DateTime.Now.ToLongTimeString();
+
+        }
+
+        private void ReiniciarButton_Click(object sender, EventArgs e)
+        {
+            Reinciar();
+        }
     }
 }
+
